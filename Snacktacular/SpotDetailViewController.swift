66915fff91c81749b2c5761ddbee21f07af9f115
Marissa <marissa.spletter@yahoo.com>
2021-10-30 11:05:46 -0400

    10.30

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -13,19 +13,56 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var ratingLabel: UILabel!
     
+    var spot: Spot!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if spot == nil {
+            spot = Spot()
+        }
+        updateFromInterface()
     }
     
-    @IBAction func cancelButtonPressed(_ sender: Any) {
+    func updateUserInterface() {
+        nameTextField.text = spot.name
+        addressTextField.text = spot.address
+        //'updateMap()
+    }
+    
+//    func updateMap() {
+//        mapView.removeAnnotations(mapView.annotations)
+//        mapView.addAnnotation(spot)
+//        mapView.setCenter(spot.coordinate, animated: true)
+//    }
+    
+    func updateFromInterface() {
+        spot.name = nameTextField.text!
+        spot.address = addressTextField.text!
+    }
+    
+    func leaveViewController() {
         let isPresentingInAddMode = presentingViewController is UINavigationController
         if isPresentingInAddMode {
             dismiss(animated: true, completion: nil)
         } else {
             navigationController?.popViewController(animated: true)
         }
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        updateFromInterface()
+        spot.saveData { (success) in
+            if success {
+                self.leaveViewController()
+            } else {
+                self.oneButtonAlert(title: "Save Failed", message: "For some reason, the data would not save to the cloud.")
+            }
+        }
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        leaveViewController()
+
     }
     
 }
