708f475204326f8943144f63a12807af7a6ec80c
Marissa <marissa.spletter@yahoo.com>
2021-11-05 14:06:58 -0400

    11.5.21

--- Snacktacular/SpotTableViewCell.swift
+++ Snacktacular/SpotTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SpotTableViewCell: UITableViewCell {
 
@@ -13,6 +14,21 @@ class SpotTableViewCell: UITableViewCell {
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
     
-    
+    var currentLocation: CLLocation!
+    var spot: Spot! {
+        didSet {
+            nameLabel.text = spot.name
+            let roundedAverage = ((spot.averageRating * 10).rounded()) / 10
+            ratingLabel.text = "Avg. Rating: \(roundedAverage)"
+            
+            guard let currentLocation = currentLocation else {
+                distanceLabel.text = "Distance: -.-"
+                return
+            }
+            let distanceInMeters = spot.location.distance(from: currentLocation)
+            let distanceInMiles = ((distanceInMeters * 0.00062137) * 10).rounded() / 10
+            distanceLabel.text = "Distance: \(distanceInMiles) miles"
+        }
+    }
     
 }
